9f5664e852d1a4e077f5c17ff5c0adcd6bb348f8
kenolendzion <kenolendzion@gmail.com>
2022-06-05 22:28:07 +0200

    Recipe Inital.

---
+++ EderiaRecipeDB/Domain/Entities/Recipe.cs
@@ -0,0 +1,12 @@
+﻿
+namespace Domain.Entities
+{
+    public class Recipe : BaseAuditableEntity 
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int KcalPerPortion { get; set; }
+        public int CookingTimeInMinutes { get; set; }   
+    }
+}
