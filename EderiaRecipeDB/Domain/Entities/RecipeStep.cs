dedae50ca2c85603bbb2304409bf681e214ee74d
kenolendzion <kenolendzion@gmail.com>
2022-06-05 22:28:54 +0200

    RecipeStep Initial.

---
+++ EderiaRecipeDB/Domain/Entities/RecipeStep.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class RecipeStep : BaseAuditableEntity
+    {
+        public Guid Id { get; set; }
+        public Guid RecipeId { get; set; }
+        public string InstructionText { get; set; }
+        public int SequenceNumber { get; set; }
+    }
+}
