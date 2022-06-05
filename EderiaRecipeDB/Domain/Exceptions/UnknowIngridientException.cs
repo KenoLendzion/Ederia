aa533370336fde8797fe01aef75ceb16325990f6
kenolendzion <kenolendzion@gmail.com>
2022-06-05 22:30:29 +0200

    UnknownRecipeException Initial.

---
+++ EderiaRecipeDB/Domain/Exceptions/UnknowIngridientException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Exceptions
+{
+    public class UnknowIngridientException : Exception 
+    {
+        public UnknowIngridientException(Ingridient ingridient) : base($"Ingridient {ingridient.Id} does't exist.")
+        {
+
+        }
+    }
+}
