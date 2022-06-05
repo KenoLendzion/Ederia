f42d905a88ad8d6a9ec41620e429d1013773a6e7
kenolendzion <kenolendzion@gmail.com>
2022-06-05 22:28:32 +0200

    RecipeIngridient Initial.

---
+++ EderiaRecipeDB/Domain/Entities/RecipeIngridient.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class RecipeIngridient : BaseAuditableEntity
+    {
+        public Guid RecipeId { get; set; }
+        public Guid IngridientId { get; set; }
+        public int Amount { get; set; }
+    }
+}
