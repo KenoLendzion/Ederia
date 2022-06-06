c167ba7667f21c5e3631491e48c2a14d7ffa2566
kenolendzion <kenolendzion@gmail.com>
2022-06-06 18:25:53 +0200

    CreateRecipeIngridientCommandValidator Initial.

---
+++ EderiaRecipeDB/Application/RecipeIngridients/Commands/CreateRecipeIngridient/CreateRecipeIngridientCommandValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+namespace Application.RecipeIngridients.Commands.CreateRecipeIngridient
+{
+    public class CreateRecipeIngridientCommandValidator : AbstractValidator<CreateRecipeIngridientCommand>
+    {
+        public CreateRecipeIngridientCommandValidator()
+        {
+            RuleFor(v => v.id).NotEmpty();
+            RuleFor(v => v.RecipeId).NotEmpty();
+            RuleFor(v => v.IngridientId).NotEmpty();
+        }
+    }
+}
