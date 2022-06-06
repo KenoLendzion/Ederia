e1c9c38ec048910eabbb23d13988c847fc0cee5b
kenolendzion <kenolendzion@gmail.com>
2022-06-06 20:58:34 +0200

    CreateRecipeStepCommandValidator Initial.

---
+++ EderiaRecipeDB/Application/RecipeSteps/Commands/CreateRecipeStep/CreateRecipeStepCommandValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+
+namespace Application.RecipeSteps.Commands.CreateRecipeStep
+{
+    public class CreateRecipeStepCommandValidator : AbstractValidator<CreateRecipeStepCommand>
+    {
+        public CreateRecipeStepCommandValidator()
+        {
+            RuleFor(v => v.Id).NotEmpty();
+            RuleFor(v => v.RecipeId).NotEmpty();
+            RuleFor(v => v.InstructionText).NotEmpty();
+            RuleFor(v => v.SequenceNumber).NotEmpty();
+        }
+    }
+}
