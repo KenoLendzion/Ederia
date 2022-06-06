a481e9d7ff1dcd853dddac4528a52f4534a5efcd
kenolendzion <kenolendzion@gmail.com>
2022-06-06 18:53:41 +0200

    CreateRecipeStepCommand Initial.

---
+++ EderiaRecipeDB/Application/RecipeSteps/Commands/CreateRecipeStep/CreateRecipeStepCommand.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Application.Common.Interfaces;
+using MediatR;
+using Domain.Entities;
+using Domain.Events;
+
+namespace Application.RecipeSteps.Commands.CreateRecipeStep
+{
+    public record CreateRecipeStepCommand : IRequest<Guid>
+    {
+        public Guid Id { get; set; }
+        public Guid RecipeId { get; set; }
+        public string InstructionText { get; set; }
+        public int SequenceNumber { get; set; }
+    }
+
+    public class CreateRecipeStepCommandHandler : IRequestHandler<CreateRecipeStepCommand, Guid>
+    {
+        private readonly IApplicationDbContext _context;
+        public async Task<Guid> Handle(CreateRecipeStepCommand request, CancellationToken cancellationToken)
+        {
+            var entity = new RecipeStep
+            {
+                Id = request.Id,
+                RecipeId = request.RecipeId,
+                InstructionText = request.InstructionText,
+                SequenceNumber = request.SequenceNumber
+            };
+
+            entity.AddDomainEvent(new RecipeStepCreatedEvent(entity));
+
+            _context.RecipeSteps.Add(entity);
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return entity.Id;
+        }
+    }
+
+}
