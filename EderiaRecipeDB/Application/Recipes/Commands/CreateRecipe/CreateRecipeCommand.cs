0f31a48b5e095f5da05226d238dbd440de70e85f
kenolendzion <kenolendzion@gmail.com>
2022-06-06 18:26:17 +0200

    CreateRecipeCommand Initial.

---
+++ EderiaRecipeDB/Application/Recipes/Commands/CreateRecipe/CreateRecipeCommand.cs
@@ -0,0 +1,34 @@
+﻿using Application.Common.Interfaces;
+using Domain.Entities;
+using Domain.Events;
+using MediatR;
+
+namespace Application.Recipes.Commands.CreateRecipe
+{
+    public record CreateRecipeCommand : IRequest<Guid>
+    {
+        public Guid Id { get; set; }
+        public string? Name { get; set; }
+    }
+
+    public class CreateRecipeCommandHandler : IRequestHandler<CreateRecipeCommand, Guid> 
+    {
+        private readonly IApplicationDbContext _context;
+
+        public async Task<Guid> Handle(CreateRecipeCommand request, CancellationToken cancellationToken)
+        {
+            var entity = new Recipe
+            {
+                Id = request.Id,
+                Name = request.Name,
+            };
+
+            entity.AddDomainEvent(new RecipeCreatedEvent(entity));
+
+            _context.Recipes.Add(entity);
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return entity.Id;
+        }
+    }
+}
