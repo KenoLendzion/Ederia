6c6a1bde7d9db964038f7b8d344657e60ca43d07
kenolendzion <kenolendzion@gmail.com>
2022-06-06 18:09:28 +0200

    IApplicationDbContext Initial.

---
+++ EderiaRecipeDB/Application/Common/Interfaces/IApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Common.Interfaces
+{
+    public interface IApplicationDbContext 
+    {
+        DbSet<Recipe> Recipes { get; }
+        DbSet<Ingridient> Ingridients { get; }
+        DbSet<RecipeIngridient> RecipeIngridients { get; }
+        DbSet<RecipeStep> RecipeSteps { get; }
+
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+    }
+}
