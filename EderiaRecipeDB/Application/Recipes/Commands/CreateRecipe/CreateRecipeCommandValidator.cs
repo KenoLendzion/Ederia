06b9677d352e0c6c993ebe539b252b68c2d16905
kenolendzion <kenolendzion@gmail.com>
2022-06-06 18:26:49 +0200

    CreateRecipeCommandValidator Initial.

---
+++ EderiaRecipeDB/Application/Recipes/Commands/CreateRecipe/CreateRecipeCommandValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+
+namespace Application.Recipes.Commands.CreateRecipe
+{
+    public class CreateRecipeCommandValidator : AbstractValidator<CreateRecipeCommand>
+    {
+        public CreateRecipeCommandValidator()
+        {
+            // Kind of useless since Id can/should never be null anyway.
+            RuleFor(v => v.Id).NotEmpty();
+
+            // 300 is a bit high. I might change it later.
+            RuleFor(v => v.Name).MaximumLength(300);
+        }
+    }
+}
