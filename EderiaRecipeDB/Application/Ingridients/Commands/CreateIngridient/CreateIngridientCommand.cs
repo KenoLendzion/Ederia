65540a62b1bc38dd4f026f3d4cb589e5b20d5ed3
kenolendzion <kenolendzion@gmail.com>
2022-06-06 18:09:50 +0200

    CreateIngridientCommand Initial.

---
+++ EderiaRecipeDB/Application/Ingridients/Commands/CreateIngridient/CreateIngridientCommand.cs
@@ -0,0 +1,33 @@
+﻿using Application.Common.Interfaces;
+using Domain.Entities;
+using Domain.Events;
+using MediatR;
+
+namespace Application.Ingridients.Commands.CreateIngridient
+{
+    public  record CreateIngridientCommand : IRequest<Guid>
+    {
+        public Guid Id { get; set; }
+        public string? Name { get; set; }
+    }
+
+    public class CreateIngridientCommandHandler : IRequestHandler<CreateIngridientCommand, Guid>
+    {
+        private readonly IApplicationDbContext _context;
+        public async Task<Guid> Handle(CreateIngridientCommand request, CancellationToken cancellationToken)
+        {
+            var entity = new Ingridient
+            {
+                Id = request.Id,
+                Name = request.Name,
+            };
+
+            entity.AddDomainEvent(new IngridientCreatedEvent(entity));
+
+            _context.Ingridients.Add(entity);
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return entity.Id;
+        }
+    }
+}
