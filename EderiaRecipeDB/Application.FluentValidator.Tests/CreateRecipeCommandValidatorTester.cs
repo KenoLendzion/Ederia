7eef7e73c581ed81cdfd6d9b56d074ab59e23ad5
kenolendzion <kenolendzion@gmail.com>
2022-06-07 22:09:25 +0200

    Application.FluentValidation.Tests Initial.

---
+++ EderiaRecipeDB/Application.FluentValidator.Tests/CreateRecipeCommandValidatorTester.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using FluentValidation;
+using FluentValidation.TestHelper;
+using Application.Recipes.Commands.CreateRecipe;
+using System;
+
+namespace Application.FluentValidation.Tests
+{
+    [TestFixture]
+    public class CreateRecipeCommandValidatorTester
+    {
+        private CreateRecipeCommandValidator validator;
+
+        [SetUp]
+        public void SetUp()
+        {
+            validator = new CreateRecipeCommandValidator();
+        }
+
+        [Test]
+        public void Should_have_error_when_Name_is_longer_than_300_Characters()
+        {
+            var model = new CreateRecipeCommand
+            {
+                Id = Guid.NewGuid(),
+                Name = "abcdefghijcdefghijababcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghij"
+            };
+            var result = validator.TestValidate(model);
+            result.ShouldHaveValidationErrorFor(model => model.Name);
+        }
+
+    }
+}
