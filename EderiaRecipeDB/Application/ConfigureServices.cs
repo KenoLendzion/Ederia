10fe92682425972694b80f0fba65431c05ba931e
kenolendzion <kenolendzion@gmail.com>
2022-06-13 22:37:25 +0200

    ConfigureServices Initial + Installed NuGets.

---
+++ EderiaRecipeDB/Application/ConfigureServices.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation;
+using MediatR;
+    public static class ConfigureServices
+    {
+        public static IServiceCollection AppApplicationServices(this IServiceCollection services)
+        {
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+            
+            //Add Behaviors here
+
+        return services;
+        }
+    }
+
