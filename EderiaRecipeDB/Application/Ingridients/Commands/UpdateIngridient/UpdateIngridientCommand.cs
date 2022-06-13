f17534d0543001741d0160c08f189dfbf1e73490
kenolendzion <kenolendzion@gmail.com>
2022-06-13 22:03:33 +0200

    UpdateIngridientCommand Initial.

---
+++ EderiaRecipeDB/Application/Ingridients/Commands/UpdateIngridient/UpdateIngridientCommand.cs
@@ -0,0 +1,36 @@
+﻿using Application.Common.Exceptions;
+using Application.Common.Interfaces;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Ingridients.Commands.UpdateIngridient
+{
+    public record UpdateIngridientCommand : IRequest
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+
+    public class UpdateIngridientCommandHandler : IRequestHandler<UpdateIngridientCommand, Unit>
+    {
+        IApplicationDbContext _context;
+        public async Task<Unit> Handle(UpdateIngridientCommand request, CancellationToken cancellationToken)
+        {
+            var entity = await _context.Ingridients.FindAsync(request.Id, cancellationToken);
+            if (entity == null)
+            {
+                throw new NotFoundException(nameof(entity), request.Id);
+            }
+
+            entity.Name = request.Name; 
+            entity.Description = request.Description;
+
+            return Unit.Value;
+        }
+    }
+}
