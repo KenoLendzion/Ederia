22475ed205a9589b64b3ba0f08655a2cd4e636d3
kenolendzion <kenolendzion@gmail.com>
2022-06-14 22:34:46 +0200

    RecipeStep query files created.

---
+++ EderiaRecipeDB/Application/RecipeSteps/Queries/Get/GetRecipeStepById.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.RecipeSteps.Queries.Get
+{
+    internal class GetRecipeStepById
+    {
+        // TODO GetRecipeStepById
+    }
+}
