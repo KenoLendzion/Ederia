3120698c6d523a4efa6ef9987e69bdffc9a990ca
kenolendzion <kenolendzion@gmail.com>
2022-06-14 22:33:46 +0200

    Recipe query files created.

---
+++ EderiaRecipeDB/Application/Recipes/Queries/Get/GetRecipeById.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Recipes.Queries.Get
+{
+    internal class GetRecipeById
+    {
+        // TODO GetRecipeById
+    }
+}
