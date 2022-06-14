0c51d659c8d8ce959588e1af1e1c12257fbb8f92
kenolendzion <kenolendzion@gmail.com>
2022-06-14 22:30:29 +0200

    Ingridients Query Files created.

---
+++ EderiaRecipeDB/Application/Ingridients/Queries/Get/GetIngridientById.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Ingridients.Queries.Get
+{
+    internal class GetIngridientById
+    {
+        // TODO GetIngridientById
+    }
+}
