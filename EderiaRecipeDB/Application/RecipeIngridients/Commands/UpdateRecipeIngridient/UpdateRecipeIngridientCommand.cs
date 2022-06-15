4f06ebe17b5b7d522d6d507055f9df0c82a21af7
kenolendzion <kenolendzion@gmail.com>
2022-06-15 23:04:33 +0200

    UpdateRecipeIngridientCommand Initial Immplementaion Fixes #13

--- EderiaRecipeDB/Application/RecipeIngridients/Commands/UpdateRecipeIngridient/UpdateRecipeIngridientCommand.cs
+++ EderiaRecipeDB/Application/RecipeIngridients/Commands/UpdateRecipeIngridient/UpdateRecipeIngridientCommand.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Application.Common.Exceptions;
+using Application.Common.Interfaces;
+using MediatR;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,39 @@ using System.Threading.Tasks;
 
 namespace Application.RecipeIngridients.Commands.UpdateRecipeIngridient
 {
-    internal class UpdateRecipeIngridientCommand
+    public record UpdateRecipeIngridientCommand : IRequest
     {
+        public int Id { get; set; }
+        public int IngridientId { get; set; }
+        public int RecipeId { get; set; }
+        public int Amount { get; set; }
+    }
+
+    public class UpdateRecipeIngridientCommandHandler : IRequestHandler<UpdateRecipeIngridientCommand, Unit>
+    {
+        IApplicationDbContext _context;
+
+        public UpdateRecipeIngridientCommandHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Unit> Handle(UpdateRecipeIngridientCommand request, CancellationToken cancellationToken)
+        {
+            var entity = await _context.RecipeIngridients.FindAsync(new object[] { request.Id }, cancellationToken);
+
+            if (entity == null)
+            {
+                throw new NotFoundException(nameof(request), request.Id);
+            }
+
+            entity.IngridientId = request.IngridientId;
+            entity.RecipeId = request.RecipeId; 
+            entity.Amount = request.Amount;
+
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return Unit.Value;
+        }
     }
 }
