acc5492a130a3e54f7f83719eb1955dd81ed2a4f
kenolendzion <kenolendzion@gmail.com>
2022-06-15 22:56:05 +0200

    Created file UpdateRecipeIngridientCommand.

---
+++ EderiaRecipeDB/Application/RecipeIngridients/Commands/UpdateRecipeIngridient/UpdateRecipeIngridientCommand.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.RecipeIngridients.Commands.UpdateRecipeIngridient
+{
+    internal class UpdateRecipeIngridientCommand
+    {
+    }
+}
