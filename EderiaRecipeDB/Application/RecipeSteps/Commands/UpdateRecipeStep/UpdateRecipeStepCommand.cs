4d7434a90567b0cc1137b298dc940fd9aed1a682
kenolendzion <kenolendzion@gmail.com>
2022-06-15 22:54:21 +0200

    UpdateRecipeStepCommand Initial implementation. Fixes #8

--- EderiaRecipeDB/Application/RecipeSteps/Commands/UpdateRecipeStep/UpdateRecipeStepCommand.cs
+++ EderiaRecipeDB/Application/RecipeSteps/Commands/UpdateRecipeStep/UpdateRecipeStepCommand.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Application.Common.Exceptions;
+using Application.Common.Interfaces;
+using MediatR;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +9,39 @@ using System.Threading.Tasks;
 
 namespace Application.RecipeSteps.Commands.UpdateRecipeStep
 {
-    internal class UpdateRecipeStepCommand
+    public record UpdateRecipeStepCommand : IRequest 
     {
-        // TODO Add UpdateRecipeStepCommand
+        public int Id { get; set; }
+        public int RecipeId { get; set; }
+        public string InstructionText { get; set; }
+        public int SquenceNumber { get; set; }
+    }
+
+    public class UpdateRecipeStepCommandHandler : IRequestHandler<UpdateRecipeStepCommand, Unit>
+    {
+        IApplicationDbContext _context;
+
+        public UpdateRecipeStepCommandHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Unit> Handle(UpdateRecipeStepCommand request, CancellationToken cancellationToken)
+        {
+            var entity = await _context.RecipeSteps.FindAsync(new object[] { request.Id },cancellationToken );
+
+            if (entity == null)
+            {
+                throw new NotFoundException(nameof(request), request.Id);            
+            }
+
+            entity.RecipeId = request.RecipeId;
+            entity.InstructionText = request.InstructionText;
+            entity.SequenceNumber = request.SquenceNumber;
+
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return Unit.Value;
+        }
     }
 }
