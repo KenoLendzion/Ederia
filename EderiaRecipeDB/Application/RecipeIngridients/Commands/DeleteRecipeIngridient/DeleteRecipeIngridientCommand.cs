e692f1ed47d6101f944158c55b55dd072e156863
kenolendzion <kenolendzion@gmail.com>
2022-06-18 23:20:25 +0200

    DeleteRecipeIngridentCommand Initial.

--- EderiaRecipeDB/Application/RecipeIngridients/Commands/DeleteRecipeIngridient/DeleteRecipeIngridientCommand.cs
+++ EderiaRecipeDB/Application/RecipeIngridients/Commands/DeleteRecipeIngridient/DeleteRecipeIngridientCommand.cs
@@ -1,13 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Application.Common.Exceptions;
+using Application.Common.Interfaces;
+using Domain.Events;
+using MediatR;
 
 namespace Application.RecipeIngridients.Commands.DeleteRecipeIngridient
 {
-    internal class DeleteRecipeIngridientCommand
+    public record DeleteRecipeIngridientCommand : IRequest
     {
-        // TODO DeleteRecipeIngridientCommand
+        public int Id { get; set; }
+    }
+
+    public class DeleteRecipeIngridientCommandHandler : IRequestHandler<DeleteRecipeIngridientCommand>
+    {
+        IApplicationDbContext _context;
+
+        public DeleteRecipeIngridientCommandHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Unit> Handle(DeleteRecipeIngridientCommand request, CancellationToken cancellationToken)
+        {
+            var entity = await _context.RecipeIngridients.FindAsync(new object[] {request.Id}, cancellationToken);
+
+            if( entity == null )
+            {
+                throw new NotFoundException();
+            }
+
+            _context.RecipeIngridients.Remove(entity);
+            entity.AddDomainEvent(new RecipeIngridientDeletedEvent(entity));
+
+            await _context.SaveChangesAsync(cancellationToken);
+            return Unit.Value;
+        }
     }
 }
