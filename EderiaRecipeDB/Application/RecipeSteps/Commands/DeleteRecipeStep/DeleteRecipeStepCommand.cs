a81dd0fb4e5e50e2d7161b1579a96a027a27381b
kenolendzion <kenolendzion@gmail.com>
2022-06-18 22:58:30 +0200

    DeleteRecipeStepCommand file created.

---
+++ EderiaRecipeDB/Application/RecipeSteps/Commands/DeleteRecipeStep/DeleteRecipeStepCommand.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.RecipeSteps.Commands.DeleteRecipeStep
+{
+    internal class DeleteRecipeStepCommand
+    {
+        // TODO DeleteRecipeStepCommand
+    }
+}
