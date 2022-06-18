0ea71066242cdacede56518721dad6a2f78961c6
kenolendzion <kenolendzion@gmail.com>
2022-06-18 23:04:40 +0200

    DeleteRecipeIngridientCommand file created.

---
+++ EderiaRecipeDB/Application/RecipeIngridients/Commands/DeleteRecipeIngridient/DeleteRecipeIngridientCommand.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.RecipeIngridients.Commands.DeleteRecipeIngridient
+{
+    internal class DeleteRecipeIngridientCommand
+    {
+        // TODO DeleteRecipeIngridientCommand
+    }
+}
