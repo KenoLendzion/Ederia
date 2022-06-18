6e9cab475a1e2c53f15251246ea2b2cf34876342
kenolendzion <kenolendzion@gmail.com>
2022-06-18 23:33:34 +0200

    DeleteIngridientCommand Initial.

--- EderiaRecipeDB/Application/Ingridients/Commands/DeleteIngridient/DeleteIngridientCommand.cs
+++ EderiaRecipeDB/Application/Ingridients/Commands/DeleteIngridient/DeleteIngridientCommand.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Application.Common.Exceptions;
+using Application.Common.Interfaces;
+using Domain.Events;
+using MediatR;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +10,33 @@ using System.Threading.Tasks;
 
 namespace Application.Ingridients.Commands.DeleteIngridient
 {
-    internal class DeleteIngridientCommand
+    public record DeleteIngridientCommand : IRequest
     {
-        // TODO DeleteIngridientCommand
+        public int Id { get; set; }
+    }
+
+    public class DeleteIngridentCommandHandler : IRequestHandler<DeleteIngridientCommand>
+    {
+        IApplicationDbContext _context;
+
+        public DeleteIngridentCommandHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Unit> Handle(DeleteIngridientCommand request, CancellationToken cancellationToken)
+        {
+            var enttity = await _context.Ingridients.FindAsync(new object[] { request.Id }, cancellationToken);
+            
+            if( enttity == null )
+            {
+                throw new NotFoundException();
+            }
+
+            _context.Ingridients.Remove(enttity);
+            enttity.AddDomainEvent(new IngridientDeletedEvent(enttity));
+
+            return Unit.Value;
+        }
     }
 }
