41fdc313f83da3ef5d029bc0e491b1f29a2fa925
kenolendzion <kenolendzion@gmail.com>
2022-06-19 11:25:26 +0200

    IngridientDeletedEvent added BaseEvent Fixes #36.

--- EderiaRecipeDB/Domain/Events/IngridientDeletedEvent.cs
+++ EderiaRecipeDB/Domain/Events/IngridientDeletedEvent.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Domain.Events
 {
-    public class IngridientDeletedEvent
+    public class IngridientDeletedEvent : BaseEvent
     {
         public Ingridient Ingridient { get; set; }
         public IngridientDeletedEvent(Ingridient ingridient)
