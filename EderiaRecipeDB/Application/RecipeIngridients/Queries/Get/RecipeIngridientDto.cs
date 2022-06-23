3af15e9980dd31d771227f2e268145a29abab367
kenolendzion <kenolendzion@gmail.com>
2022-06-23 18:47:10 +0200

    RecipeIngridientDto Intitial. Fixed #40

---
+++ EderiaRecipeDB/Application/RecipeIngridients/Queries/Get/RecipeIngridientDto.cs
@@ -0,0 +1,18 @@
+﻿using Application.Common.Mappings;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.RecipeIngridients.Queries.Get
+{
+    public class RecipeIngridientDto : IMapFrom<RecipeIngridient>
+    {
+        public int Id { get; set; }
+        public int RecipeId { get; set; }
+        public int IngridientId { get; set; }
+        public int? Amount { get; set; }
+    }
+}
