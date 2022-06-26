9c47dacf9c389edd8226d200a1f601688b1fbad8
kenolendzion <kenolendzion@gmail.com>
2022-06-26 21:26:57 +0200

    MediatorExtentions Initial. Fixed #53

---
+++ EderiaRecipeDB/Infrastructure/Common/MediatorExtentions.cs
@@ -0,0 +1,31 @@
+﻿using Domain.Common;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Common
+{
+    public static class MediatorExtentions
+    {
+        public static async Task DispatchDomainEvents(this IMediator mediator, DbContext context)
+        {
+            var entities = context.ChangeTracker
+                .Entries<BaseEntity>()
+                .Where(e => e.Entity.DomainEvents.Any())
+                .Select(e => e.Entity);
+
+            var domainEvents = entities
+                .SelectMany(e => e.DomainEvents)
+                .ToList();
+
+            foreach( var domainEvent in domainEvents )
+            {
+                await mediator.Publish(domainEvent);
+            }
+        }
+    }
+}
