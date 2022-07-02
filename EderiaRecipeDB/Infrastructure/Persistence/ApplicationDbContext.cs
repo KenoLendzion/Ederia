2b36b08ca11bb54c85af2c9748d91dfa8e43e9fa
kenolendzion <kenolendzion@gmail.com>
2022-07-02 21:13:59 +0200

    ApplicationDbContext Initial. Fixes #50

---
+++ EderiaRecipeDB/Infrastructure/Persistence/ApplicationDbContext.cs
@@ -0,0 +1,56 @@
+﻿using Application.Common.Interfaces;
+using Domain.Entities;
+using Duende.IdentityServer.EntityFramework.Options;
+using Infrastructure.Common;
+using Infrastructure.Identity;
+using Infrastructure.Persistence.Interceptors;
+using MediatR;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System.Reflection;
+
+namespace Infrastructure.Persistence
+{
+    public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>, IApplicationDbContext
+    {
+        private readonly IMediator _mediator;
+        private readonly AuditableEntitySaveChangesInterceptor _auditableEntitySaveChangesInterceptor;
+
+        public ApplicationDbContext(
+            DbContextOptions<ApplicationDbContext> options,
+            IOptions<OperationalStoreOptions> operationalStoreOptions,
+            IMediator mediator,
+            AuditableEntitySaveChangesInterceptor auditableEntitySaveChangesInterceptor) : base(options, operationalStoreOptions)    
+        {
+            _mediator = mediator;
+            _auditableEntitySaveChangesInterceptor = auditableEntitySaveChangesInterceptor;
+        }
+
+
+        public DbSet<Recipe> Recipes => Set<Recipe>();
+
+        public DbSet<Ingridient> Ingridients => Set<Ingridient>();
+
+        public DbSet<RecipeIngridient> RecipeIngridients => Set<RecipeIngridient>();
+
+        public DbSet<RecipeStep> RecipeSteps => Set<RecipeStep>();
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            base.OnModelCreating(builder);
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.AddInterceptors(_auditableEntitySaveChangesInterceptor);
+        }
+
+        public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            await _mediator.DispatchDomainEvents(this);
+            return await base.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
