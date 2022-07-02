1eedf645342346ef548d5f2f27cc61e8f90ce6fd
kenolendzion <kenolendzion@gmail.com>
2022-07-02 21:54:39 +0200

    Initial. Fixes #44

---
+++ EderiaRecipeDB/Infrastructure/ConfigureServices.cs
@@ -0,0 +1,56 @@
+﻿using Application.Common.Interfaces;
+using Infrastructure.Identity;
+using Infrastructure.Persistence;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure
+{
+    public static class ConfigureServices 
+    {
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            // TODO AuditableEntitySaveChangesInterceptor
+
+            if(configuration.GetValue<bool>("UseInMemoryDatabse"))
+            {
+                services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseInMemoryDatabase("EderiaDb"));
+            }
+            else
+            {
+                services.AddDbContext<ApplicationDbContext>(options =>
+                    options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"),
+                        builder => builder.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)));
+            }
+
+            services.AddScoped<IApplicationDbContext>(provider => provider.GetRequiredService<ApplicationDbContext>());
+
+            services.AddScoped<ApplicationDbContextInitialiser>();
+
+            services.AddDefaultIdentity<ApplicationUser>()
+                .AddRoles<IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>();
+
+            services.AddIdentityServer()
+                .AddApiAuthorization<ApplicationUser, ApplicationDbContext>();
+
+            // TODO services.AddTransient<IdentityServiceCollectionExtensions, IdentityServices>();
+
+            // TODO AddIdentityServerJWT
+
+            // TODO AddPolicy
+            //services.AddAuthentication(options =>
+            //    options.AddPolicy("CanPurge", policy => policy.RequireRole("Administator")));
+
+            return services;
+        }
+    }
+}
