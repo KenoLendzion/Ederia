b53800644d0cff5703cf5c0770fc4c538afc7999
kenolendzion <kenolendzion@gmail.com>
2022-07-02 21:34:57 +0200

    ApplicationDbContextInitialiser Initial. Fixes #51

---
+++ EderiaRecipeDB/Infrastructure/Persistence/ApplicationDbContextInitialiser.cs
@@ -0,0 +1,73 @@
+﻿using Infrastructure.Identity;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Persistence
+{
+    public class ApplicationDbContextInitialiser
+    {
+        private readonly ILogger<ApplicationDbContextInitialiser> _logger;
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public ApplicationDbContextInitialiser(ILogger<ApplicationDbContextInitialiser> logger, 
+            ApplicationDbContext context, 
+            UserManager<ApplicationUser> userManager,
+            RoleManager<IdentityRole> roleManager)
+        {
+            _logger = logger;
+            _context = context;
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+
+        public async Task InitializeAsync()
+        {
+            try
+            {
+                if(_context.Database.IsSqlServer())
+                {
+                    await _context.Database.MigrateAsync();
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "An error occured while initialising the database.");
+                throw;
+            }
+        }
+
+        public async Task TrySeedAsync()
+        {
+            var administratorRole = new IdentityRole("Administrator");
+
+            if(_roleManager.Roles.All(r => r.Name != administratorRole.Name))
+            {
+                await _roleManager.CreateAsync(administratorRole);
+            }
+
+            var administrator = new ApplicationUser { UserName = "administrator@localhost", EmailTokenProvider = "administrator@localhost" };
+
+            if(_userManager.Users.All(u => u.UserName != administrator.UserName))
+            {
+                await _userManager.CreateAsync(administrator, "Administator1!");
+                await _userManager.AddToRolesAsync(administrator, new[] { administratorRole.Name });
+            }
+
+            if(!_context.Recipes.Any())
+            {
+                _context.Recipes.Add(new Domain.Entities.Recipe
+                {
+                    Name = "TestRecipe",
+                    Description = "This is a Test Recipe",
+                    KcalPerPortion = 124,
+                    CookingTimeInMinutes = 24
+                });
+            }
+
+            await _context.SaveChangesAsync();
+        }
+    }
+}
